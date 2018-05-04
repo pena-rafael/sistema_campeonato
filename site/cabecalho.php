a4f1b07df93b85554b56895751152942fa33c99b
Rafael Pena <rafamaic1337@gmail.com>
2018-05-04 15:16:40 -0300

    Atualização cabeçalho

--- site/cabecalho.php
+++ site/cabecalho.php
@@ -4,15 +4,21 @@
 		<title>Sistema para Campeonatos</title>
 	<head>
 	<body>
-		<?php 
-		if(!isset($_SESSION["logado"])){
-		?>
 		<nav>
-			<a href="index.php">Início</a>	|	
-			<a href="cadastro.php">Cadastrar</a>	|	
+			<?php
+			if(!isset($_SESSION["logado"])){
+			?>
+			<a href="index.php">Início</a>	|
+			<a href="cadastro.php">Cadastrar</a>	|
 			<a href="login.php">Login</a>
+			<?php
+			} else{
+			?>
+			<a href="index.php">Início</a>	|
+			<a href="cadastrar_campeonato.php">Cadastro Campeonato</a>	|
+			<a href="ver_campeonatos.php">Ver Meus Campeonatos</a>	|
+			<a href="faq.php">FAQ</a>	|
+			<a href="sair.php">Sair</a>
+		<?php } ?>
 		</nav>
 		<content>
-		<?php 
-		} else{
-		?>
