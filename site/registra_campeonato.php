b97457892a00286f9bc7c011cdba8c4a6d021f8c
Danielli Teixeira <dannyteixeira955@gmail.com>
2018-06-20 23:37:24 -0300

    Alteração do cadastro do campeonato

--- site/registra_campeonato.php
+++ site/registra_campeonato.php
@@ -8,7 +8,8 @@ if(isset($_POST["tipo"])){
   $nome_campeonato = $_POST["nome_campeonato"];
   $tipo = $_POST["tipo"];
   $id_usuario = $_SESSION["usuario"];
-  cadastro_campeonato($nome_campeonato, $tipo, $id_usuario);
+  $desc_campeonato = $_SESSION["desc_campeonato"];
+  cadastro_campeonato($nome_campeonato, $tipo, $id_usuario, $desc_campeonato);
   foreach($_POST["nome_time"] as $i=>$v) {
     //$jogadores = $_POST["nome_jogador".$i];
     cadastro_time($v);
